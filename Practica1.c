c201a076a5ee4bcd346a3237e955815802667bac
MontiF <miguelmonteagudoferia@gmail.com>
2024-10-30 13:35:51 +0100

    practica1

--- Practica1.c
+++ Practica1.c
@@ -7,7 +7,7 @@ int main() {
 	float suma, promedio; 
 
 
-	char nombre_dias[7][11] = {"lunes","martes","miercoles","jueves","viernes","sabado","domingo"};	//un array en el cual se guardan cadenas de caracteres
+	char nombre_dias[7][11] = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};	//un array en el cual se guardan cadenas de caracteres
 	float dias[7] = {l, m, x, j, v, s, d};	//un array con los dias de la semana
 	for(int i = 0; i<7;i++){
 		printf("¿Que temperatura habia el %s?\n",nombre_dias[i]);		//un printf que cambia la palabra del dia gracias al array anterior
@@ -17,11 +17,51 @@ int main() {
 	for(int i = 0; i < 7; i++){
 		suma += dias[i];						//calcula el promedio 
 	} promedio = suma / 7;
-	
-	printf("La temperatura promedio es: %f\n", promedio);
+
+
+	printf("La temperatura promedio de la semana es: %.1fCº\n", promedio);
 
 	
+	//calcula maxima
+	int dia_maxima;
+	float maxima = dias[0];   
+	for(int i = 0; i < 7; i++){
+		if(dias[i] >= maxima){				//si la avariable maxima es menor o y a dias[i] entonces la variable maxima sera igual a dias[i]
+			maxima = dias[i];
+			dia_maxima = i;	
+	}
+	} printf("Temperatura máxima: %.1fCº el día %s \n", maxima, nombre_dias[dia_maxima]);
 	
+	
+	//calcula minimo
+	int dia_minimo;
+	float minima = dias [0];
+	for(int i = 0; i < 7; i++){
+		if(dias[i] <= minima){
+			minima = dias[i];
+			dia_minimo = i;
+		}
+	}printf("Temperatura mínima: %.1fCº el día %s \n", minima, nombre_dias[dia_minimo]);
+
+	//numero de dias con temperaturas superiores al promedio
+	int contador_mayores = 0;
+	for(int i = 0; i < 7; i++){
+		if(dias[i] > promedio){
+			contador_mayores++;
+		}
+		
+	}printf("Número de días con temperaturas superiores al promedio: %d\n", contador_mayores);
+
+	//dias con temperaturas por debajo del promedio
+			printf("Días con temperaturas por debajo del promedio:\n");
+	for(int i = 0;i < 7; i++){
+		if(dias[i] < promedio){
+			printf("%s: %.1fCº\n", nombre_dias[i], dias[i]);
+		}
+	}
+	
+	
+
 	return 0;
 
 }
