161a1b6b6d9cb481192d69aa0e2fdbb8d47052b2
MontiF <miguelmonteagudoferia@gmail.com>
2024-10-30 14:02:23 +0100

    pract1

--- Practica1.c
+++ Practica1.c
@@ -3,12 +3,11 @@
 
 int main() {
 	
-	float l, m, x, j, v, s, d;		
 	float suma, promedio; 
 
 
 	char nombre_dias[7][11] = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};	//un array en el cual se guardan cadenas de caracteres
-	float dias[7] = {l, m, x, j, v, s, d};	//un array con los dias de la semana
+	float dias[7] ;	//un array con los dias de la semana
 	for(int i = 0; i<7;i++){
 		printf("¿Que temperatura habia el %s?\n",nombre_dias[i]);		//un printf que cambia la palabra del dia gracias al array anterior
 		scanf("%f", &dias[i]);							//un scanf que cambia la variable declarada dependiendo de las veces que haga el bucle
@@ -24,24 +23,21 @@ int main() {
 	
 	//calcula maxima
 	int dia_maxima;
-	float maxima = dias[0];   
-	for(int i = 0; i < 7; i++){
-		if(dias[i] >= maxima){				//si la avariable maxima es menor o y a dias[i] entonces la variable maxima sera igual a dias[i]
-			maxima = dias[i];
-			dia_maxima = i;	
-	}
-	} printf("Temperatura máxima: %.1fCº el día %s \n", maxima, nombre_dias[dia_maxima]);
-	
-	
-	//calcula minimo
 	int dia_minimo;
 	float minima = dias [0];
+	float maxima = dias[0];   
 	for(int i = 0; i < 7; i++){
-		if(dias[i] <= minima){
-			minima = dias[i];
+		if(dias[i] >= maxima){				//si la avariable maxima es menor o = a dias[i] entonces la variable maxima sera igual a dias[i]
+			maxima = dias[i];			// y guardaremos la [i] para mas tarde indicar el dia
+			dia_maxima = i;	
+	}
+		if(dias[i] <= minima){				//si la variable minima es mayor o = a dias[i] entonces la variable maxima sera igual a dias[i]
+			minima = dias[i];			// y guardaremos la [i] para más tarde indicar día
 			dia_minimo = i;
 		}
-	}printf("Temperatura mínima: %.1fCº el día %s \n", minima, nombre_dias[dia_minimo]);
+	} 
+	printf("Temperatura máxima: %.1fCº el día %s \n", maxima, nombre_dias[dia_maxima]);
+	printf("Temperatura mínima: %.1fCº el día %s \n", minima, nombre_dias[dia_minimo]);
 
 	//numero de dias con temperaturas superiores al promedio
 	int contador_mayores = 0;
