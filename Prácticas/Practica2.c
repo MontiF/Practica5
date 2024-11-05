7ca50406b47d484095867e969058ef1151c35924
lucas061971 <lucasmediaroble@gmail.com>
2024-11-05 14:30:46 +0100

    seguir

--- Prácticas/Practica2.c
+++ Prácticas/Practica2.c
@@ -1,3 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main() {
+	
+	int estudiantes;
+	int num_estudiante{estudiantes};
+	int calificacion{estudiantes};
+
+	printf("Ingreses una cantidad de estudiantes:");
+	scanf("%d",&estudiantes);
+	for(int i = 0; i <= estudiantes, i++){
+		printf("Pon la nota del estudiante %d:",i++);
+		scanf("%d", &calificacion[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
