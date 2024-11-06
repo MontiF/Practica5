e4cb7f5dfe62479d1f81d6deb3677091e2e9fab3
lucas061971 <lucasmediaroble@gmail.com>
2024-11-06 12:13:21 +0100

    avances

---
+++ Ejercicios/ej1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (){
+	int producto;
+	int nom_producto;
+	int inventario[continente][sede][categoría][modelo][lote];
+
+
+	printf("Cuantos productos quieres gestionar?:%d\n");
+	scanf("%d\n",&producto);
+
--- Prácticas/Practica2.c
+++ Prácticas/Practica2.c
@@ -2,17 +2,56 @@
 #include <stdlib.h>
 
 int main() {
-	
+	int i = 0;
 	int estudiantes;
-	int num_estudiante{estudiantes};
-	int calificacion{estudiantes};
+	double suma = 0;
+	int aprobados = 0;
+	int suspensos = 0;
 
-	printf("Ingreses una cantidad de estudiantes:");
-	scanf("%d",&estudiantes);
-	for(int i = 0; i <= estudiantes, i++){
-		printf("Pon la nota del estudiante %d:",i++);
-		scanf("%d", &calificacion[i])
+	printf("Ingrese una cantidad de estudiantes:");
+	scanf("%d", &estudiantes);
 
+	double calificacion[estudiantes];
+	double minima = calificacion[0];
+	double maxima = calificacion[0];
+	int persona_maxima;
+	int persona_minima;
+
+
+
+	for(i = 0; i < estudiantes; i++){
+		int real = i + 1;
+		printf("Pon la nota del estudiante %d:", real);
+		scanf("%lf", &calificacion[i]);
+		printf("nota %lf estudiante %d\n", calificacion[i], real);
+		suma = suma + calificacion[i];
+		
+		if ( calificacion[i] >= 5){
+			aprobados = aprobados + 1;
+		} else {
+			suspensos = suspensos + 1;
+		}
+		
+		if ( calificacion[i] >= maxima) {
+			maxima = calificacion[i];
+			persona_maxima = i + 1;
+		}
+		
+		if ( calificacion[i] <= minima) {
+			minima = calificacion[i];
+			persona_minima = i + 1;
+		}
+} 
+
+		double promedio = suma / estudiantes;
+		printf("El promedio de notas: %.2lf\n", promedio);
+
+	printf("Número de estudiantes aprobados: %d\n", aprobados);
+	printf("Número de estudiantes suspendidos: %d\n", suspensos);
+	printf("Nota máxima: %lf estudiante número: %d\n", maxima, persona_maxima);
+	printf("Nota mínima: %lf estudainte número: %d\n", minima, persona_minima);
+
+		
 
 
 
